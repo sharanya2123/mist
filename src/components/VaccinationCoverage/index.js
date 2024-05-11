b3710257b074518a3a96918a2a618343148036db
sharanya2123 <nxtwave@ip-10-2-52-104.ap-south-1.compute.internal>
2024-05-11 09:29:24 +0000

    mist

---
+++ src/components/VaccinationCoverage/index.js
@@ -0,0 +1,71 @@
+// Write your code here
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
+
+import './index.css'
+
+const VaccinationCoverage = props => {
+  const dataFormatter = number => {
+    if (number > 1000) {
+      return `${(number / 1000).toString()}k`
+    }
+    return number.toString()
+  }
+
+  const {vaccinationCoverageDetails} = props
+
+  return (
+    <div className="vaccination-by-coverage-container">
+      <h1 className="vaccination-by-coverage-heading">Vaccination Coverage</h1>
+      <BarChart
+        width={900}
+        height={400}
+        data={vaccinationCoverageDetails}
+        margin={{
+          top: 5,
+        }}
+      >
+        <XAxis
+          dataKey="vaccineData"
+          tick={{
+            stroke: '#6c757d',
+            strokenWidth: 1,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <YAxis
+          ticketFormatter={dataFormatter}
+          tick={{
+            stroke: '#6c757d',
+            strokenWidth: 0.5,
+            fontSize: 15,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            paddingTop: 20,
+            textAlign: 'center',
+            fontSize: 12,
+            fontFamily: 'Roboto',
+          }}
+        />
+        <Bar
+          dataKey="dose1"
+          name="Dose 1"
+          radius={[10, 10, 0, 0]}
+          barSize="20%"
+        />
+        <Bar
+          dataKey="dose 2"
+          name="Dose 2"
+          fill="#f54394"
+          radius={[5, 5, 0, 0]}
+          barSize="20%"
+        />
+      </BarChart>
+    </div>
+  )
+}
+
+export default VaccinationCoverage
